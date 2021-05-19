0e5ebe85555a58faff0a652559a6c6df0f9875b7
Justin Lee <j.lee@qfab.org>
2021-05-19 14:32:25 +1000

    added script for clearing expired apollo backups from OpenStack object store (to be run with cron)

---
+++ backup_scripts/swift-backup-delete-old.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# delete backups from Apollo_SQL_Archive container when they reach 37 days old (5 weeks 2 days)
+# Note: this should be run after backup-apollos.sh in the crontab
+
+BACKUP_ROOT=/mnt/backup00
+BACKUP_BIN=$BACKUP_ROOT/scripts
+APOLLO_ARCHIVE_COUNT=$(ls -ld $BACKUP_ROOT/apollo-???_archive | wc -l)
+
+SWIFT_CRED_FILE=$HOME/app-cred-swift-openrc.sh
+SWIFT_PATH=$HOME/.local
+EXPIRE=37
+KEEP=$APOLLO_ARCHIVE_COUNT
+CONTAINER="Apollo_SQL_Archive"
+
+$BACKUP_BIN/deleteOldSwiftObjects.sh -v -p $SWIFT_PATH -c $SWIFT_CRED_FILE -e $EXPIRE -k $KEEP $CONTAINER
+

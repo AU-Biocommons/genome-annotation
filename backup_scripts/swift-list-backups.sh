bcb4a83cc948526974c33581b5c8c74a359f3fe8
Justin Lee <j.lee@qfab.org>
2021-05-19 14:33:17 +1000

    added script for listing apollo backups in OpenStack object store

---
+++ backup_scripts/swift-list-backups.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# list backups in Apollo_SQL_Archive container
+
+BACKUP_ROOT=/mnt/backup00
+BACKUP_BIN=$BACKUP_ROOT/scripts
+
+SWIFT_CRED_FILE=$HOME/app-cred-swift-openrc.sh
+SWIFT_PATH=$HOME/.local
+CONTAINER="Apollo_SQL_Archive"
+
+$BACKUP_BIN/listSwift.sh -p $SWIFT_PATH -c $SWIFT_CRED_FILE $CONTAINER
+

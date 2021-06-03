7eb7334607178be0882e7784a470ba6b6e465b24
Justin Lee <j.lee@qfab.org>
2021-06-03 12:03:02 +1000

    add support for running tomcat on smaller memory VMs in test environment through setfacts

--- ansible/roles/common-tomcat-ubuntu20/templates/setenv.sh
+++ ansible/roles/common-tomcat-ubuntu20/templates/setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export PERL5LIB=/opt/Apollo-2.6.0/extlib/lib/perl5:$PERL5LIB
-export CATALINA_OPTS="-Xms8g -Xmx12g"
+export CATALINA_OPTS="{{apollo_tomcat_memory_opts}}"
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 

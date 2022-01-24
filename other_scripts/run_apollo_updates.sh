3ccf12c678d6497c66bc4f2cb0867f1a504a29df
Justin Lee <j.lee@qfab.org>
2022-01-24 15:15:39 +1000

    added script that automates running all the apollo system updates

---
+++ other_scripts/run_apollo_updates.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Note: ansible-sandpit should be manually updated with:"
+echo "        $ sudo apt update; sudo apt upgrade -y"
+echo "      and rebooted if needed with:"
+echo "        $ sudo shutdown -r now"
+echo
+
+pushd . > /dev/null
+
+echo "change to playbooks directory to run system updates on VMs..."
+cd ~/github-ansible/ansible/playbooks/
+pwd
+echo
+
+echo "run system updates on centos infrastructure and test VMs (this may involve reboots)"
+echo "updating apollo-monitor, apollo-backup and centos-test..."
+echo "ansible-playbook playbook-system-updates-centos.yml --limit centosvms"
+ansible-playbook playbook-system-updates-centos.yml --limit centosvms
+
+echo "run system updates on ubuntu infrastructure and test VMs (this may involve reboots)"
+echo "updating apollo-data, apollo-portal, ubuntu18-test and ubuntu20-test..."
+echo "ansible-playbook playbook-system-updates-ubuntu.yml --limit otherubuntuvms"
+ansible-playbook playbook-system-updates-ubuntu.yml --limit otherubuntuvms
+
+echo "run system updates on all apollo VMs (this may involve reboots)"
+echo "ansible-playbook playbook-system-updates-ubuntu.yml --limit apollovms"
+ansible-playbook playbook-system-updates-ubuntu.yml --limit apollovms
+
+echo "system updates completed!"
+echo "check that all systems are up and running from Nagios:"
+echo "    http://nagios.genome.edu.au/nagios/"
+
+popd > /dev/null
+

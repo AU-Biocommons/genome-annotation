b023161fa7a7e9e37652432b4c7bb4ba9b6e01cb
Justin Lee <j.lee@qfab.org>
2022-08-01 17:22:17 +1000

    added check for 'system restart required' when running run_apollo_updates.sh on ansible server

--- other_scripts/run_apollo_updates.sh
+++ other_scripts/run_apollo_updates.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "Note: ansible-sandpit should be manually updated with:"
-echo "        $ sudo apt update; sudo apt upgrade -y"
-echo "      and rebooted if needed with:"
-echo "        $ sudo shutdown -r now"
-echo
-
 pushd . > /dev/null
 
 echo "change to playbooks directory to run system updates on VMs..."
@@ -13,15 +7,33 @@ cd ~/github-ansible/ansible/playbooks/
 pwd
 echo
 
-echo "run system updates on centos infrastructure and test VMs (this may involve reboots)"
-echo "updating apollo-backup and centos-test..."
-echo "ansible-playbook playbook-system-updates-centos.yml --limit centosvms"
-ansible-playbook playbook-system-updates-centos.yml --limit centosvms
+# when a reboot is required, the file /var/run/reboot-required will exist
+# the contents of this is what gives the MOTD "*** System restart required ***"
+# via /etc/update-motd.d/98-reboot-required
+if [ -f /var/run/reboot-required ]; then
+    echo "WARNING: System restart required on ansible server - running system updates first:"
+    echo "  $ sudo apt update; sudo apt upgrade -y"
+    sudo apt update
+    sudo apt upgrade -y
+    echo
+    echo "System updates completed."
+    echo
+    echo "Please save work in progress and then manually reboot this server with:"
+    echo "  'sudo shutdown -r'"
+    echo "Then re-run this script to run updates across other hosts."
+    echo
+    exit 0
+fi
 
 echo "run system updates on ubuntu infrastructure and test VMs (this may involve reboots)"
 echo "updating apollo-data, apollo-monitor, apollo-portal and ubuntu20-test..."
 echo "ansible-playbook playbook-system-updates-ubuntu.yml --limit otherubuntuvms"
 ansible-playbook playbook-system-updates-ubuntu.yml --limit otherubuntuvms
+
+echo "run system updates on centos infrastructure and test VMs (this may involve reboots)"
+echo "updating apollo-backup and centos-test..."
+echo "ansible-playbook playbook-system-updates-centos.yml --limit centosvms"
+ansible-playbook playbook-system-updates-centos.yml --limit centosvms
 
 echo "run system updates on all apollo VMs (this may involve reboots)"
 echo "ansible-playbook playbook-system-updates-ubuntu.yml --limit apollovms"

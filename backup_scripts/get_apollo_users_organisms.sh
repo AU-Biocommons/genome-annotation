315a3cacfcba96113f77a1fe557161dd65a7e42a
Justin Lee <j.lee@qfab.org>
2024-05-21 14:46:26 +1000

    add client tokens to report script

--- backup_scripts/get_apollo_users_organisms.sh
+++ backup_scripts/get_apollo_users_organisms.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 ORGANISMS_COUNT="SELECT COUNT(1) FROM organism;"
+ORGANISMS_PUBLISHED_COUNT="SELECT COUNT(public_mode) FROM organism WHERE public_mode='t';"
 ORGANISMS_QUERY="SELECT common_name, genus, species, public_mode FROM organism;"
 USERS_COUNT="SELECT COUNT(1) FROM grails_user;"
 USERS_QUERY="SELECT grails_user.id, role.name AS role_name, inactive, first_name, last_name, username FROM grails_user JOIN grails_user_roles ON grails_user.id = grails_user_roles.user_id JOIN role ON grails_user_roles.role_id = role.id;"
+TOKENS_COUNT="SELECT COUNT(DISTINCT client_token) AS client_token_count FROM preference;"
+TOKENS_LATEST="SELECT COALESCE((SELECT 'last update on ' || last_updated::date as update_day FROM preference ORDER BY update_day DESC LIMIT 1), '-') AS result;"
 
 LIST_OF_INSTANCES="/mnt/backup00/list_of_apollo_instances.txt"
 
@@ -11,12 +14,19 @@ while read REMOTE_HOST; do
     #echo "$REMOTE_HOST"
     #echo ""
     echo -n "$REMOTE_HOST Organisms: "
-    psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$ORGANISMS_COUNT"
+    psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$ORGANISMS_COUNT" | tr -d '\n'
+    echo -n "; published: "
+    psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$ORGANISMS_PUBLISHED_COUNT"
     psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$ORGANISMS_QUERY"
     echo ""
     echo -n "$REMOTE_HOST Users: "
     psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$USERS_COUNT"
     psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$USERS_QUERY"
+    echo ""
+    echo -n "$REMOTE_HOST Client tokens: "
+    psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$TOKENS_COUNT" | tr -d '\n'
+    echo -n "; "
+    psql -d apollo-production -h $REMOTE_HOST -U backup_user -tAc "$TOKENS_LATEST"
     echo ""
     echo "-----------------"
     echo ""

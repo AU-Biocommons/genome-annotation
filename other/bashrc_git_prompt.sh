7c0ca1bcb8083d2c6ac2d311fb6759786d94a71c
Justin Lee <j.lee@qfab.org>
2025-05-14 12:02:29 +1000

    git prompt for bashrc

---
+++ other/bashrc_git_prompt.sh
@@ -0,0 +1,18 @@
+# add git branch info to prompt if inside a git repo
+function parse_git_branch
+{
+#       ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+#       #echo "("${ref#refs/heads/}")"
+       # Note:  this requires Bash programmable completion features enabled
+       echo $(__git_ps1 " (%s)")
+}
+
+# add GIT branch (if in a Git repo) to end of prompt and show repo status
+if [ -f /usr/share/git/git-prompt.sh ]; then
+    source /usr/share/git/git-prompt.sh
+    export GIT_PS1_SHOWDIRTYSTATE=1
+    export GIT_PS1_SHOWUNTRACKEDFILES=1
+    PS1="[\u@\h \W]\[\033[01;30m\]\$(parse_git_branch)\[\033[00m\]"
+    PS1="$PS1\$ "
+fi
+

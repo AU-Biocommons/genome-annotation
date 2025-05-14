30b1fa94d4f38eebe2263e5cd9d94a3cb9483505
Justin Lee <j.lee@qfab.org>
2025-05-14 12:27:19 +1000

    fix git prompt for ubuntu 24.04

--- other/bashrc_git_prompt.sh
+++ other/bashrc_git_prompt.sh
@@ -8,11 +8,13 @@ function parse_git_branch
 }
 
 # add GIT branch (if in a Git repo) to end of prompt and show repo status
-if [ -f /usr/share/git/git-prompt.sh ]; then
-    source /usr/share/git/git-prompt.sh
+if [ -f /etc/bash_completion.d/git-prompt ]; then
+#if [ -f /usr/share/git/git-prompt.sh ]; then
+    #source /usr/share/git/git-prompt.sh
+    source /etc/bash_completion.d/git-prompt
     export GIT_PS1_SHOWDIRTYSTATE=1
     export GIT_PS1_SHOWUNTRACKEDFILES=1
-    PS1="[\u@\h \W]\[\033[01;30m\]\$(parse_git_branch)\[\033[00m\]"
+    PS1="\u@\h \W\[\033[01;30m\]\$(parse_git_branch)\[\033[00m\]"
     PS1="$PS1\$ "
 fi
 

699573576a755f114f93605869bf75bc9c7669bf
Justin Lee <j.lee@qfab.org>
2025-06-09 11:33:04 +1000

    add terraform for apollo-portal

--- terraform-nectar/servers-nectar.tf
+++ terraform-nectar/servers-nectar.tf
@@ -1,6 +1,9 @@
 # Terraform managed servers: tfs_apollo_NAME_YYYYMMDD
 
-# backup/deploy server VM
+
+
+##########---------- backup/deploy server VM ----------##########
+
 resource "openstack_compute_instance_v2" "tfs_apollo_backup_20250430" {
     name              = "tfs_apollo_backup_20250430"
     flavor_name       = "m3.large"
@@ -73,7 +76,9 @@ resource "openstack_compute_volume_attach_v2" "apollo_backups_vol" {
 */
 
 
-# user NFS server VM
+
+##########---------- user data NFS server VM ----------##########
+
 resource "openstack_compute_instance_v2" "tfs_apollo_user_nfs_20250512" {
     name              = "tfs_apollo_user_nfs_20250512"
     flavor_name       = "m3.large"
@@ -136,7 +141,10 @@ resource "openstack_compute_volume_attach_v2" "apollo_user_data_vol" {
 }
 */
 
-# monitoring server VM
+
+
+##########---------- monitoring server VM ----------##########
+
 resource "openstack_compute_instance_v2" "tfs_apollo_monitor_20250529" {
     name              = "tfs_apollo_monitor_20250529"
     flavor_name       = "m3.medium"
@@ -188,5 +196,62 @@ data "openstack_networking_port_v2" "apollo_monitor_port" {
 resource "openstack_networking_floatingip_associate_v2" "apollo_monitor_fip_assoc" {
     floating_ip = openstack_networking_floatingip_v2.apollo_monitor_fip.address
     port_id     = data.openstack_networking_port_v2.apollo_monitor_port.id
+}
+
+
+
+##########---------- apollo-portal web server VM ----------##########
+
+resource "openstack_compute_instance_v2" "tfs_apollo_portal_20250609" {
+    name              = "tfs_apollo_portal_20250609"
+    flavor_name       = "m3.small"
+    key_pair          = "apollo-nectar"
+    availability_zone = "ardc-syd-1"
+
+    block_device {          # define boot volume with custom size
+        uuid                  = "49f677df-01e5-45c9-9611-609ef21f60e1" # NeCTAR Ubuntu 24.04 LTS (Noble) amd64
+        source_type           = "image"
+        volume_size           = 50
+        boot_index            = 0
+        destination_type      = "volume"
+        delete_on_termination = true
+    }
+
+    network {
+        name =        "apollo-internal-network"  
+    }
+
+    security_groups = [
+        "default",
+        "SSH_access",
+        "NRPE_local_access",
+        "ICMP_local_access",
+        "Postgresql_Server_local_access",
+        "Web_Server_access_full"
+    ]
+
+    metadata = {
+        description = "Apollo Portal web server"
+    }
+
+    lifecycle {
+        prevent_destroy = true
+    }
+}
+
+# create floating IP in ardc-syd IP pool
+resource "openstack_networking_floatingip_v2" "apollo_portal_fip" {
+    pool = "ardc-syd"  # IP in ardc-syd availability zone IP pool
+}
+
+# fetch the port for VM instance based on the fixed IP
+data "openstack_networking_port_v2" "apollo_portal_port" {
+    fixed_ip = openstack_compute_instance_v2.tfs_apollo_portal_20250609.network[0].fixed_ip_v4
+}
+
+# attach floating IP using the VM port ID
+resource "openstack_networking_floatingip_associate_v2" "apollo_portal_fip_assoc" {
+    floating_ip = openstack_networking_floatingip_v2.apollo_portal_fip.address
+    port_id     = data.openstack_networking_port_v2.apollo_portal_port.id
 }
 
